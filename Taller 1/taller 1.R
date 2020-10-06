c48f7e9bf4134395de63311e36ef65f0fc4b8322
jorgeluis8ar <jorgeochoa@Jorges-MacBook-Pro.local>
2020-10-05 22:33:25 -0500

    part 1.3 done

--- Taller 1/taller 1.R
+++ Taller 1/taller 1.R
@@ -109,14 +109,14 @@ resto2020 = merge(x = cg_2020_resto , y = de_2020_resto , by = c('directorio','s
 resto2020 = merge(x = resto2020 , y = ft_2020_resto , by = c('directorio','secuencia_p','orden'), all.x = T )
 resto2020 = merge(x = resto2020 , y = in_2020_resto , by = c('directorio','secuencia_p','orden'), all.x = T ) 
 resto2020 = merge(x = resto2020 , y = oc_2020_resto , by = c('directorio','secuencia_p','orden'), all.x = T )
-resto2020$periodo=2019
+resto2020$periodo=2020
 
 resto2020 %>% summarise(total=sum(fex_c_2011))
 
 cabecera=plyr::rbind.fill(cabecera2019,cabecera2020)
 resto=plyr::rbind.fill(resto2019,resto2020)
 
-
+ 
 rm(list=ls()[grepl(pattern = "cabecera2", x = ls())])
 rm(list=ls()[grepl(pattern = "resto2", x = ls())])
 rm(list=ls()[grepl(pattern = "2020_cabecera", x = ls())])
@@ -129,7 +129,41 @@ rm(list=ls()[grepl(pattern = "geih", x = ls())])
 rm(list=ls()[grepl(pattern = "n", x = ls())])
 rm(list=ls()[grepl(pattern = "paquetes", x = ls())])
 
-eliminar=c('cabecera2','resto2','2020_cabecera','2019_cabecera','2020_resto','2019_resto','lista','file','geih','n','paquetes')
 
+nacional = plyr::rbind.fill(cabecera,resto)
+lapply(nacional, function(x) is.na(x) %>% table()) 
+# Nivel
+nacional=mutate(nacional,nivel=ifelse(test = (is.na(Cabecera.Ca))==T,yes = Resto.Ca,no = Cabecera.Ca))
+nacional %>% group_by(periodo,nivel) %>% summarise(total=sum(fex_c_2011))
+# desocupados
+names(nacional)[names(nacional) == "Car"] <- "general"
+names(nacional)[names(nacional) == "Des"] <- "desocupados"
+names(nacional)[names(nacional) == "Ocu"] <- "ocupados"
+names(nacional)[names(nacional) == "Ina"] <- "inactivos"
+names(nacional)[names(nacional) == "Fue"] <- "fuerza"
 
+nombres=c(grep('secuencia_p',colnames(nacional)),
+          grep('orden',colnames(nacional)),
+          grep('directorio',colnames(nacional)),
+          grep('p6020',colnames(nacional)),
+          grep('p6040',colnames(nacional)),
+          grep('p6030s1',colnames(nacional)),
+          grep('p6050',colnames(nacional)),
+          grep('dpto',colnames(nacional)),
+          grep('nivel',colnames(nacional)),
+          grep('fex_c_2011',colnames(nacional)),
+          grep('esc',colnames(nacional)),
+          grep('mes',colnames(nacional)),
+          grep('fuerza',colnames(nacional)),
+          grep('desocupados',colnames(nacional)),
+          grep('general',colnames(nacional)),
+          grep('inactivos',colnames(nacional)),
+          grep('ocupados',colnames(nacional)),
+          grep('periodo',colnames(nacional))
+)
+
+nacional=nacional[,nombres]
+
+cabecera %>% summarise(total=sum(fex_c_2011))
+resto %>% summarise(total=sum(fex_c_2011))
 
